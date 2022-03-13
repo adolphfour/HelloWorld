e128b9fa0ed8a92986968d1a3aa253506b655dda
ADF <adolphfour@gmail.com>
2022-03-13 15:58:56 +0700

    main structure4

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -13,14 +13,24 @@ public class HelloWorld {
         System.out.println("number : "+num);
 
 
-        System.out.println("hello adolphfour");
-        System.out.println("number : "+num);
+//        System.out.println("hello adolphfour");
+//        System.out.println("number : "+num);
 
         double price1 = 5.6;
         System.out.println("\nprice = "+price1);
 
+        double Total =100.00;
+        double Vat =7.00;
+        double Grandtotal =107.00;
+        String namee = "฿";
 
 
+//        System.out.println("\nTotal "+Total+namee);
+        System.out.println("\nTotal "+Total+" ฿");
+//        System.out.printf(namee+"\n");
+
+        System.out.println("Vat7% "+Vat+" ฿");
+        System.out.println("Grand total "+Grandtotal+" ฿");
 
 
 
