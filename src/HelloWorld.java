dd918f998c1d588c1743210244f7766c07a2f784
ADF <adolphfour@gmail.com>
2022-03-13 15:14:42 +0700

    main structure2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,24 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.printf("hello adolphfour");
+        System.out.println("hello adolphfour");
+
+        int num =5;
+
+        double price = 12.34;
+
+        String name = "Samart";
+
+        boolean isCondition = false;
+
+        System.out.println("number : "+num);
+
+
+        System.out.println("hello adolphfour");
+        System.out.println("number : "+num);
+
+
+
+
+
     }
 }
